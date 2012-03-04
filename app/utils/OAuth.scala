b7b9900db2ae52c45c0d81f428f662e26c1b4875
Gorka Maiztegi <gmaiztegi@gmail.com>
2012-03-05 00:55:31 +0100

    Login uses cookies now

--- app/utils/OAuth.scala
+++ app/utils/OAuth.scala
@@ -35,6 +35,8 @@ object OAuth {
     }
     
     def Authenticated[A](action: Authentication => Action[A]): Action[(Action[A], A)] = Authenticated(
-        req => req.session.get(access_token).map(Authentication.findByToken(_)).getOrElse(None),
+        req => req.cookies.get(access_token).map{ cookie =>
+            Authentication.findByToken(cookie.value)
+        }.getOrElse(None),
         _ => Unauthorized(views.html.defaultpages.unauthorized()))(action)
 }

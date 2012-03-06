945aa2531921b117828839e917eb04d6f0889747
Gorka Maiztegi <gmaiztegi@gmail.com>
2012-03-06 02:06:26 +0100

    Added Posgresql as a dependency

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-        "com.amazonaws" % "aws-java-sdk" % "1.3.2"
+        "com.amazonaws" % "aws-java-sdk" % "1.3.2",
+        "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(

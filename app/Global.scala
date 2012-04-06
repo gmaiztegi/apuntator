fad920c1a00853435dd78124d5ecca62cbd1f256
Gorka Maiztegi <gmaiztegi@gmail.com>
2012-04-06 14:55:27 +0200

    Tasks are cancelled on stop to avoid errors

--- app/Global.scala
+++ app/Global.scala
@@ -1,6 +1,9 @@
+import scala.collection.mutable.{MutableList}
+
 import play.api._
 import play.api.libs.concurrent._
 
+import akka.actor._
 import akka.util._
 
 import java.util.concurrent.{TimeUnit}
@@ -8,15 +11,20 @@ import java.util.concurrent.{TimeUnit}
 import models._
 
 object Global extends GlobalSettings {
-    override def onStart(app: Application) {
-        Logger.info("Application has started")
 
+    private val tasks: MutableList[Cancellable] = new MutableList()
+
+    override def onStart(app: Application) {
         val delay = Duration(10, TimeUnit.SECONDS)
         val period = Duration(30, TimeUnit.MINUTES)
 
-        Akka.system(app).scheduler.schedule(delay, period) {
+        tasks += Akka.system(app).scheduler.schedule(delay, period) {
             val howmany = AccessToken.deleteExpired
             Logger.debug("Removed "+howmany+" expired access tokens.")
         }
-    }  
+    }
+
+    override def onStop(app: Application) {
+        tasks.foreach (_.cancel)
+    }
 }

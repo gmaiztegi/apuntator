3cd1c1ad3cc492734b35d57af9465ec5c3e795b6
Gorka Maiztegi <gmaiztegi@gmail.com>
2012-04-06 04:16:53 +0200

    Made code cleaner in Global.scala

--- app/Global.scala
+++ app/Global.scala
@@ -1,14 +1,18 @@
 import play.api._
 import play.api.libs.concurrent._
 
+import akka.util._
+
+import java.util.concurrent.{TimeUnit}
+
 import models._
 
 object Global extends GlobalSettings {
     override def onStart(app: Application) {
         Logger.info("Application has started")
 
-        val delay = akka.util.Duration(10, java.util.concurrent.TimeUnit.SECONDS)
-        val period = akka.util.Duration(30, java.util.concurrent.TimeUnit.MINUTES)
+        val delay = Duration(10, TimeUnit.SECONDS)
+        val period = Duration(30, TimeUnit.MINUTES)
 
         Akka.system(app).scheduler.schedule(delay, period) {
             val howmany = AccessToken.deleteExpired
